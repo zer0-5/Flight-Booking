b1baa8a39dee0efefe0f849af0ec88c0140276db
gonaloppcc <a93286@alunos.uminho.pt>
2022-01-01 19:14:31 +0000

    More documentation.

--- FlightBooking/common/airport/Flight.java
+++ FlightBooking/common/airport/Flight.java
@@ -10,14 +10,30 @@ import java.util.UUID;
  */
 public class Flight {
     // TODO: Lock;
+
+    /**
+     * Id of the flight.
+     */
     public final UUID id;
+
+    /**
+     * Route of the flight.
+     */
     public final Route route;
+
+    /**
+     * Date of the flight.
+     */
     public final LocalDate date;
 
-    // This is necessary because when a flight is canceled, we need
-    // to know all reservations associated to cancel then.
-    // Like, if a connection flight is canceled, we need to cancel the flights associated to that connection
-    private final Set<UUID> reservations; // Association each reservation code to the flight.
+    /**
+     * Association between each reservation code to the flight.
+     *
+     * This is necessary because when a flight is canceled, we need
+     * to know all reservations associated to cancel then.
+     * Like, if a connection flight is canceled, we need to cancel the flights associated to that connection
+     */
+    private final Set<UUID> reservations;
 
     /**
      * Constructor of the flight.
