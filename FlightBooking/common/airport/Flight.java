71bc2e8f3f7d4f77f2087242807231eddccf56c6
gonaloppcc <a93286@alunos.uminho.pt>
2022-01-01 19:06:47 +0000

    More documentation.

--- FlightBooking/common/airport/Flight.java
+++ FlightBooking/common/airport/Flight.java
@@ -6,37 +6,47 @@ import java.util.Set;
 import java.util.UUID;
 
 /**
- * Voo individual, pronto para ser realizado, ou já realizado.
+ * Represents a flight.
  */
 public class Flight {
     // TODO: Lock;
     public final UUID id;
     public final Route route;
     public final LocalDate date;
-    //private int remainingCapacity;
-    // private final Set<UUID> clients; // Association each client to the reservation code of his flight.
 
     // This is necessary because when a flight is canceled, we need
     // to know all reservations associated to cancel then.
     // Like, if a connection flight is canceled, we need to cancel the flights associated to that connection
     private final Set<UUID> reservations; // Association each reservation code to the flight.
 
+    /**
+     * Constructor of the flight.
+     * @param id the id.
+     * @param route the route.
+     * @param date the date.
+     */
     public Flight(UUID id, Route route, LocalDate date) {
         this.id = id;
         this.route = route;
         this.date = date;
-        //this.remainingCapacity = route.capacity;
         this.reservations = new HashSet<>();
     }
 
-    public void addCapacity(UUID uuid) {
-        //remainingCapacity--;
-        this.reservations.add(uuid);
+    /**
+     * Adds a reservation to this flight.
+     *
+     * @param reservationId the id of the reservation.
+     */
+    public void addReservation(UUID reservationId) {
+        this.reservations.add(reservationId);
     }
 
-    public void removeCapacity(UUID uuid) {
-        //if (remainingCapacity == 0) throw Cenas;
-        //remainingCapacity++;
-        this.reservations.remove(uuid);
+    /**
+     * Removes a reservation to this flight.
+     *
+     * @param reservationId the id of the reservation.
+     */
+    public void removeReservation(UUID reservationId) {
+        this.reservations.remove(reservationId);
     }
 }
