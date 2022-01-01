e480d1e8484a962f028e3f8760e37c270dc239d2
gonaloppcc <a93286@alunos.uminho.pt>
2022-01-01 18:57:50 +0000

    Fix tabs. Starting documentation.

--- FlightBooking/common/airport/Reservation.java
+++ FlightBooking/common/airport/Reservation.java
@@ -6,9 +6,15 @@ import java.util.UUID;
 public class Reservation {
     public final UUID reserveCode;
     public final UUID client;
-    public final Set<UUID> flights;
+    public final Set<UUID> flights; // Flights with the connections of the reservation e.g. Lisbon -> Toquio -> London
     // TODO: Adicionar capacidade aos flights depois de apagar a reservation
 
+    /**
+     * Constructor
+     * @param reserveCode a reserve code.
+     * @param client a client.
+     * @param flights set of flight's id.
+     */
     public Reservation(UUID reserveCode, UUID client, Set<UUID> flights) {
         this.reserveCode = reserveCode;
         this.client = client;
